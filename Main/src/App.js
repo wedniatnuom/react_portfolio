6cc8e9eee1e061f847128a383c470189a8a0f97e
Jeremy Johnson <jeremyjohnson@Jeremys-Mini.hsd1.ut.comcast.net>
2022-09-27 14:49:47 -0600

    added links to repositories for portfolio projects

--- Main/src/App.js
+++ Main/src/App.js
@@ -22,7 +22,6 @@ function App() {
     bgContact = "bg-info"
   }
 
-  
   return (
     <React.Fragment>
       <header>
--- Main/src/components/portfolio.js
+++ Main/src/components/portfolio.js
@@ -15,21 +15,27 @@ function Portfolio() {
             <div className="d-flex flex-column col-7">
                 <div className="m-2 border rounded" style={{ backgroundColor: "whitesmoke" }}>
                     <a href="http://dnd-here.herokuapp.com" ><button>Dungeons and Dragons App</button></a>
+                    <a href="https://github.com/Sercanbrack/dnd-right-here" className="m-2" style={{color: "lightseagreen"}}>github.com/Sercanbrack/dnd-right-here</a>
                 </div>
                 <div className="m-2 border rounded" style={{ backgroundColor: "whitesmoke" }}>
                     <a href="https://mvc-challenge722.herokuapp.com/"><button>Blog Application</button></a>
+                    <a href="https://github.com/wedniatnuom/mvc_challenge" className="m-2" style={{color: "lightseagreen"}}>github.com/wedniatnuom/mvc_challenge</a>
                 </div>
                 <div className="m-2 border rounded" style={{ backgroundColor: "whitesmoke" }}>
                     <a href="https://drive.google.com/file/d/1EXvi0o4WaCroBwWCTv83gaZMxqNEKbds/view?usp=sharing"><button> Video: Back End Routes</button></a>
+                    <a href="https://github.com/wedniatnuom/e-commerce_back_end" className="m-2" style={{color: "lightseagreen"}}>github.com/wedniatnuom/e-commerce_back_end</a>
                 </div>
                 <div className="m-2 border rounded" style={{ backgroundColor: "whitesmoke" }}>
                     <a href="https://drive.google.com/file/d/19ukQ4ywomF-RzU89Sya8bsA0V38-SpDG/view?usp=sharing"><button>Video: Command Line</button></a>
+                    <a href="https://github.com/wedniatnuom/employee_tracker" className="m-2" style={{color: "lightseagreen"}}>github.com/wedniatnuom/employee_tracker</a>
                 </div>
                 <div className="m-2 border rounded" style={{ backgroundColor: "whitesmoke" }}>
                     <a href="https://notetaker722.herokuapp.com/"><button>Note Keeper</button></a>
+                    <a href="https://github.com/wedniatnuom/note_taker" className="m-2" style={{color: "lightseagreen"}}>github.com/wedniatnuom/note_taker</a>
                 </div>
                 <div className="m-2 border rounded" style={{ backgroundColor: "whitesmoke" }}>
                     <a href="https://wedniatnuom.github.io/weather_dashboard/"><button>Weather Dashboard</button></a>
+                    <a href="https://github.com/wedniatnuom/weather_dashboard" className="m-2" style={{color: "lightseagreen"}}>github.com/wedniatnuom/weather_dashboard</a>
 
                 </div>
 
