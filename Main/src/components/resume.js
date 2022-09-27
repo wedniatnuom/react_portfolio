cee7d48112dcb336a93205ea1646b4d181e71c2a
Jeremy Johnson <jeremyjohnson@Jeremys-Mini.hsd1.ut.comcast.net>
2022-09-26 22:15:27 -0600

    Structure for Resume page built (laptop on the road)

--- Main/src/components/resume.js
+++ Main/src/components/resume.js
@@ -2,16 +2,25 @@ import React from "react";
 
 function Resume() {
     return (
-        <div>
+        <div className="d-flex flex-column align-items-center" style={{ backgroundColor: "lightseagreen" }}>
+            <div className="m-4 border rounded col-9" style={{ backgroundColor: "lightgray" }}>
+                <div className="d-flex flex-row justify-content-center">
+                    <div className="m-5 col-5 border rounded" style={{backgroundColor: "whitesmoke"}}>
+                        Top left corner
+                    </div>
+                    <div className="m-5 col-3 border rounded" style={{backgroundColor: "whitesmoke"}}>
+                        Top right corner
+                    </div>
+                </div>
+                <div className="d-flex flex-column align-items-center">
+                    <div className="m-4 col-9 border rounded" style={{backgroundColor: "whitesmoke"}}>Resume Item #1</div>
+                    <div className="m-4 col-9 border rounded" style={{backgroundColor: "whitesmoke"}}>Resume Item #2</div>
+                    <div className="m-4 col-9 border rounded" style={{backgroundColor: "whitesmoke"}}>Resume Item #3</div>
+                    <div className="m-4 col-9 border rounded" style={{backgroundColor: "whitesmoke"}}>Resume Item #4</div>
 
-
-
-<div>Resume test function</div>
-
-
-</div>
-   
-
+                </div>
+            </div>
+        </div>
     )
 };
 
