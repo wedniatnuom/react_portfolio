2f1bbffea780cffaeb78e1527f7284d5d6624158
Jeremy Johnson <jeremyjohnson@Jeremys-Mini.hsd1.ut.comcast.net>
2022-09-26 22:13:47 -0600

    contact page layout updates (laptop on the road)

--- Main/src/components/contact.js
+++ Main/src/components/contact.js
@@ -2,14 +2,14 @@ import React from "react";
 
 function Contact() {
     return (
-        <div>
-
-
-
-<div>Contact test function</div>
-
-
-</div>
+        <div className="d-flex flex-column align-items-center vh-100" style={{backgroundColor: "lightseagreen"}}>
+            <div className="d-flex flex-column align-items-center border rounded m-4 col-8" style={{backgroundColor: "lightgray"}}>
+            <input className="m-4" type="text" id="name" name="name" placeholder="Name"></input>
+            <input className="m-4" type="text" id="email" name="email" placeholder="Email"></input>
+            <input className="m-4" type="text" id="message" name="message" placeholder="Message"></input>
+            <button>Submit</button>
+            </div>
+        </div>
    
 
     )
