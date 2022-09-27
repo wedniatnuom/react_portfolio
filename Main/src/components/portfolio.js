e0e744a48acd6bf59240004e742ff5d67d10fce9
Jeremy Johnson <jeremyjohnson@Jeremys-Mini.hsd1.ut.comcast.net>
2022-09-26 22:14:32 -0600

    Portfolio page structure built (laptop on the road)

--- Main/src/components/portfolio.js
+++ Main/src/components/portfolio.js
@@ -2,15 +2,38 @@ import React from "react";
 
 function Portfolio() {
     return (
-        <div>
+        <div className="d-flex flex-column align-items-center" style={{backgroundColor: "lightseagreen"}}>
+        <div className="d-flex flex-row col-11 m-4 border rounded" style={{backgroundColor: "lightgray"}}>
+            <div className="m-4 border rounded col-4" style={{ backgroundColor: "whitesmoke" }}>
+                <div>
+                    Intro to portfolio, where the work came from, ect...
+                </div>
 
+            </div>
+            <div className="d-flex flex-column col-7">
+                <div className="m-4 border rounded" style={{ backgroundColor: "whitesmoke" }}>
+                    <div>Project 1</div>
+                </div>
+                <div className="m-4 border rounded" style={{ backgroundColor: "whitesmoke" }}>
+                    <div>Project 2</div>
+                </div>
+                <div className="m-4 border rounded" style={{ backgroundColor: "whitesmoke" }}>
+                    <div>Project 3</div>
+                </div>
+                <div className="m-4 border rounded" style={{ backgroundColor: "whitesmoke" }}>
+                    <div>Project 4</div>
+                </div>
+                <div className="m-4 border rounded" style={{ backgroundColor: "whitesmoke" }}>
+                    <div>Project 5</div>
+                </div>
+                <div className="m-4 border rounded" style={{ backgroundColor: "whitesmoke" }}>
+                    <div>Project 6</div>
+                </div>
 
+            </div>
+        </div>
+        </div>
 
-<div>Portfolio test function</div>
-
-
-</div>
-   
 
     )
 };
