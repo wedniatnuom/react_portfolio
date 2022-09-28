7943bc164bb07a71c659a992cbb3293f701fe052
Jeremy Johnson <jeremyjohnson@Jeremys-Mini.hsd1.ut.comcast.net>
2022-09-27 22:36:56 -0600

    general clean up

--- src/components/Project.js
+++ src/components/Project.js
@@ -2,7 +2,6 @@ import React from "react";
 
 
 function Project(props) {
-    console.log(props);
     return (
         <div className="m-2 border rounded" style={{ backgroundColor: "whitesmoke" }}>
             <a href={props.project.link} ><button>{props.project.title}</button></a>
