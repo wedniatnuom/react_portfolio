d4ced3c11f8e2c2c6712dc3b563d0def48b36e81
Jeremy Johnson <jeremyjohnson@Jeremys-Mini.hsd1.ut.comcast.net>
2022-09-27 22:31:08 -0600

    general clean up

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -2,7 +2,7 @@ import React from "react";
 
 function Contact() {
     return (
-        <div className="d-flex flex-column align-items-center vh-100" style={{backgroundColor: "lightseagreen"}}>
+        <div className="d-flex flex-column align-items-center" style={{backgroundColor: "lightseagreen", height: "900px"}}>
             <div className="d-flex flex-column align-items-center border rounded m-4 col-8" style={{backgroundColor: "lightgray"}}>
             <input className="m-4" title="required" type="text" id="name" name="name" placeholder="Name"></input>
             <input className="m-4" title="required" type="text" id="email" name="email" placeholder="Email"></input>
