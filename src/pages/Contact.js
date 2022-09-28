f63ffc8f3583fcf6afebda3e4b296ddde8e13e6f
Jeremy Johnson <jeremyjohnson@Jeremys-Mini.hsd1.ut.comcast.net>
2022-09-27 22:34:14 -0600

    general clean up

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -2,12 +2,12 @@ import React from "react";
 
 function Contact() {
     return (
-        <div className="d-flex flex-column align-items-center" style={{backgroundColor: "lightseagreen", height: "900px"}}>
+        <div className="d-flex flex-column align-items-center" style={{backgroundColor: "lightseagreen", height: "500px"}}>
             <div className="d-flex flex-column align-items-center border rounded m-4 col-8" style={{backgroundColor: "lightgray"}}>
-            <input className="m-4" title="required" type="text" id="name" name="name" placeholder="Name"></input>
-            <input className="m-4" title="required" type="text" id="email" name="email" placeholder="Email"></input>
-            <input className="m-4" title="required" type="text" id="message" name="message" placeholder="Message"></input>
-            <button>Submit</button>
+            <input className="m-4" title="required" type="text" id="name" name="name" placeholder="Name" style={{width: "300px"}}></input>
+            <input className="m-4" title="required" type="text" id="email" name="email" placeholder="Email" style={{width: "300px"}}></input>
+            <input className="m-4" title="required" type="text" id="message" name="message" placeholder="Message" style={{height: "200px", width: "600px"}}></input>
+            <button className="m-2">Submit</button>
             </div>
         </div>
    
