edf7ffff322bb277b06d0360ddfc6c894fbd9f30
Jeremy Johnson <jeremyjohnson@Jeremys-Mini.hsd1.ut.comcast.net>
2022-10-02 02:45:19 -0600

    fixing link still

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,7 +6,7 @@ function Footer() {
         <nav className="d-flex " >
             <div className="d-flex align-items-center col-12 justify-content-around" >
                 <a href="https://github.com/wedniatnuom" style={{textDecoration: "none", color: "black"}}><div className="text-center" >GitHub</div></a>
-                <a href="www.linkedin.com/in/jeremy-johnson-82606b237" style={{textDecoration: "none", color: "black"}}><div className="text-center" >LinkedIn</div></a>
+                <a href="https://www.linkedin.com/in/jeremy-johnson-82606b237" style={{textDecoration: "none", color: "black"}}><div className="text-center" >LinkedIn</div></a>
                 <a href="https://www.facebook.com/profile.php?id=1368658088" style={{textDecoration: "none", color: "black"}}><div className="text-center">Facebook</div></a>
             </div>
         </nav>
